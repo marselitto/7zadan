f24ed72e8be7d275144f5a770f80153e381404c8
szybek <jakub.jaworowski@cyfroweszkoly.pl>
2024-12-16 10:39:06 +0100

    Coin&Dice

--- app/src/main/java/com/example/a7zad/CoinOrDice.kt
+++ app/src/main/java/com/example/a7zad/CoinOrDice.kt
@@ -4,3 +4,19 @@ import kotlin.random.Random
 interface RollingTool{
     fun roll(): String
 }
+
+class Dice : RollingTool{
+    override fun roll(): String{
+        return (1..6).random().toString()
+    }
+}
+
+class Coin : RollingTool{
+    override fun roll(): String {
+        return when((1..2).random()) {
+            1 -> "Heads"
+            2 -> "Tails"
+            else -> "Błąd"
+        }
+    }
+}
