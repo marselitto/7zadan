07e6af0b1d4a0271c81ca2179d0e2c5526c7fd91
jj <jakub.jaworowski@cyfroweszkoly.pl>
2024-12-19 00:37:35 +0100

    Add function addBook

--- app/src/main/java/com/example/a7zad/FavBookTracker.kt
+++ app/src/main/java/com/example/a7zad/FavBookTracker.kt
@@ -2,3 +2,11 @@ package com.example.a7zad
 
 data class Book(val title: String, val author: String, val year: Int)
 
+class FavouriteBookTracker{
+    private val booksList = mutableListOf<Book>()
+
+    fun addBook(title: String, author: String, year: Int){
+        booksList.add(Book(title, author, year))
+        println("Dodano ksiazke: $title autorstwa $author z $year roku")
+    }
+}
