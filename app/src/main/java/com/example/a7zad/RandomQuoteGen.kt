5d9c0f9f6a800ac2b1fdd6686038925de4c26d73
jj <twojemail@example.com>
2024-12-21 12:48:39 +0100

    Add quotes to list

---
+++ app/src/main/java/com/example/a7zad/RandomQuoteGen.kt
@@ -0,0 +1,11 @@
+package com.example.a7zad
+
+object Quotes{
+    val quotes = mutableListOf(
+        "Czytanie książek to najpiękniejsza zabawa, jaką sobie ludzkość wymyśliła",
+        "Życie jest jak pudełko czekoladek - nigdy nie wiesz, co ci się trafi.",
+        "Pozbierać jest się dziesięć razy trudniej, niż rozsypać.",
+        "Najpiękniejszych chwil w życiu nie zaplanujesz. One przyjdą same",
+        "Każdy człowiek powinien żyć tak, by stanowić wzór dla innych. "
+    )
+}
