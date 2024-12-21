b0fe36060d7e381d1eadb4449a970c8b87e1db87
jj <twojemail@example.com>
2024-12-21 14:30:10 +0100

    Add class MultipleChoiceQuestion

--- app/src/main/java/com/example/a7zad/Quiz.kt
+++ app/src/main/java/com/example/a7zad/Quiz.kt
@@ -5,6 +5,19 @@ interface Question {
     fun checkAnswer(answer: String): Boolean
 }
 
+class MultipleChoiceQuestion(val questionText: String, val options: List<String>, val correctOption: Int) : Question {
+    override fun askQuestion(): String {
+        var askQuestion = "$questionText"
+        for (i in options.indices) {
+            askQuestion += "${i + 1}. ${options[i]}"
+        }
+        return askQuestion
+    }
+
+    override fun checkAnswer(answer: String): Boolean {
+        return answer.toInt() == correctOption
+    }
+}
 
 fun main(){
 
